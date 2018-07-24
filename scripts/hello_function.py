56cadafecfd3d25d0cde4d630c37597fee510d2d
AliMcKillip <mckillipalexandra@gmail.com>
2018-07-24 13:16:55 -0500

    adding practice scripts

---
+++ scripts/hello_function.py
@@ -0,0 +1,11 @@
+import time
+#this script demonstartes how to use try: except structure
+def hello():
+    print "Hello World!"
+    time.sleep(10)
+
+if __name__ == '__main__':
+    try:
+        hello()
+    except KeyboardInterrupt:
+        print 'Goodbye'
